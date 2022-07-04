30cdf64860b3f85c1321e87247fd2c12f7dbbe38
Shkarupa Alex <shkarupa.alex@gmail.com>
2022-07-04 17:47:56 +0300

    Some tests and refactoring

--- nlpclean/clean.py
+++ nlpclean/clean.py
@@ -1,45 +1,12 @@
+import re
+from unicodedata import normalize
+
+ACCENT_GLYPHS = re.compile('[\u0060\u02C6\u02CA\u02CB\u02CE\u02CF\u02DD\u02DF\u02F4\u02F5\u02F6\u0300\u0301\u0302]+')
 
 
-def remove_html_tags(text):
-    pass
+def remove_accent_glyphs(text):
+    text = normalize('NFD', text)
+    text = re.sub(ACCENT_GLYPHS, '', text)
+    text = normalize('NFC', text)
 
-def remove_trash_words(text):
-    pass
-
-# https://github.com/google-research/electra/blob/master/model/tokenization.py
-# def _run_strip_accents(self, text):
-#     """Strips accents from a piece of text."""
-#     text = unicodedata.normalize("NFD", text)
-#     output = []
-#     for char in text:
-#       cat = unicodedata.category(char)
-#       if cat == "Mn":
-#         continue
-#       output.append(char)
-#     return "".join(output)
-# def _clean_text(self, text):
-#     """Performs invalid character removal and whitespace cleanup on text."""
-#     output = []
-#     for char in text:
-#       cp = ord(char)
-#       if cp == 0 or cp == 0xfffd or _is_control(char):
-#         continue
-#       if _is_whitespace(char):
-#         output.append(" ")
-#       else:
-#         output.append(char)
-#     return "".join(output)
-
-
-# def _is_control(char):
-#   """Checks whether `chars` is a control character."""
-#   # These are technically control characters but we count them as whitespace
-#   # characters.
-#   if char == "\t" or char == "\n" or char == "\r":
-#     return False
-#   cat = unicodedata.category(char)
-#   if cat.startswith("C"):
-#     return True
-#   return False
-
-# TODO https://github.com/NeelShah18/emot/blob/master/emot/emo_unicode.py
+    return text
