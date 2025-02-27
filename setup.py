840eb570420957738c4a1ca92f008dc71b1fc520
Shkarupa Alex <shkarupa.alex@gmail.com>
2025-02-27 09:33:30 +0300

    Update version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='nlpclean',
-    version='1.3.1',
+    version='1.3.2',
     description='Utilities for cleaning up text corpus',
     long_description=long_description,
     long_description_content_type="text/markdown",
